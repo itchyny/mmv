dc16bb0051bb2ec769e26307d69189d1567adf98
itchyny <itchyny@hatena.ne.jp>
2020-01-07 21:32:34 +0900

    implement simple Move function

--- mmv.go
+++ mmv.go
@@ -1,1 +1,13 @@
 package mmv
+
+import "os"
+
+// Move multiple files.
+func Move(files map[string]string) error {
+	for k, v := range files {
+		if err := os.Rename(k, v); err != nil {
+			return err
+		}
+	}
+	return nil
+}
