02f7964dc55176592b08a87d9ef168060270a8b4
itchyny <itchyny@hatena.ne.jp>
2020-01-07 21:45:36 +0900

    introduce rename struct

--- mmv.go
+++ mmv.go
@@ -4,10 +4,26 @@ import "os"
 
 // Move multiple files.
 func Move(files map[string]string) error {
-	for k, v := range files {
-		if err := os.Rename(k, v); err != nil {
+	rs, err := buildRenames(files)
+	if err != nil {
+		return err
+	}
+	for _, r := range rs {
+		if err := os.Rename(r.src, r.dst); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+type rename struct {
+	src, dst string
+}
+
+func buildRenames(files map[string]string) ([]rename, error) {
+	rs := make([]rename, 0, 2*len(files))
+	for src, dst := range files {
+		rs = append(rs, rename{src, dst})
+	}
+	return rs, nil
 }
